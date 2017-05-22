42adc15cd54f8c5a79e25a20b78d7cb13d7b9414
Sam Hamilton <sam@sh81.com>
2017-05-22 19:49:52 +0800

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -29,10 +29,11 @@ defmodule Arc.Storage.GCS.Mixfile do
 
   defp deps do
     [
-      {:goth, "~> 0.4"},
-      {:sweet_xml, "~> 0.6"},
       {:arc, "~> 0.8", only: :test},
-      {:excoveralls, "~> 0.6", only: :test}
+      {:excoveralls, "~> 0.6", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:goth, "~> 0.4"},
+      {:sweet_xml, "~> 0.6"}
     ]
   end
 end
