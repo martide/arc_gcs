5e518f3fe713d3fd5b148cb0121a2c4daef6a4cf
Thanya Nitithatsanakul <ammbot@gmail.com>
2019-11-11 18:12:17 +0700

    revert to healthty state
    - GoogleApi branch break backward-compatible

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,9 @@
 use Mix.Config
 
+config :arc,
+  bucket: {:system, "ARC_BUCKET"},
+  storage: Arc.Storage.GCS
+
+config :goth, json: {:system, "GCP_CREDENTIALS"}
+
 import_config "#{Mix.env()}.exs"
