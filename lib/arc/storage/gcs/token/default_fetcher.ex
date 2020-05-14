0ef63081c2683a4e920ee5168f1923908f2d0915
James van Lommel <jamesvl@gmail.com>
2020-05-14 02:42:13 -0400

    Fixed missing alias for Goth.Token module (#101)
    Also removes an unused alias warning from gcs.ex

--- lib/arc/storage/gcs/token/default_fetcher.ex
+++ lib/arc/storage/gcs/token/default_fetcher.ex
@@ -1,6 +1,8 @@
 defmodule Arc.Storage.GCS.Token.DefaultFetcher do
   @behaviour Arc.Storage.GCS.Token.Fetcher
 
+  alias Goth.Token
+
   @impl Arc.Storage.GCS.Token.Fetcher
   def get_token(scopes) when is_list(scopes), do: get_token(Enum.join(scopes, " "))
 
