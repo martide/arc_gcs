c53fe7a546e2350e1e9e38333d3cd05ce790fa51
Daniel Flanagan <daniel@lyte.dev>
2020-12-08 13:02:10 -0600

    Update pathing and URL logic to respect virtual_host configuration

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,6 @@ config :arc,
   bucket: {:system, "ARC_BUCKET"},
   storage: Arc.Storage.GCS
 
-config :goth, json: {:system, "GCP_CREDENTIALS"}
+# config :goth, json: {:system, "GCP_CREDENTIALS"}
 
 import_config "#{Mix.env()}.exs"
