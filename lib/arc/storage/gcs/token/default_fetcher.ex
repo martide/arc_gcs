6083ce373751b0fbf19b64999ac451703543c82d
Darren Klein <darren.a.klein@gmail.com>
2021-09-14 21:14:44 -0400

    In the event of a for_scope error, returns an empty string for the token.

--- lib/arc/storage/gcs/token/default_fetcher.ex
+++ lib/arc/storage/gcs/token/default_fetcher.ex
@@ -4,10 +4,16 @@ defmodule Arc.Storage.GCS.Token.DefaultFetcher do
   alias Goth.Token
 
   @impl Arc.Storage.GCS.Token.Fetcher
-  def get_token(scopes) when is_list(scopes), do: get_token(Enum.join(scopes, " "))
+  def get_token(scopes) when is_list(scopes) do
+    scopes
+    |> Enum.join(" ")
+    |> get_token()
+  end
 
   def get_token(scope) when is_binary(scope) do
-    {:ok, token} = Token.for_scope(scope)
-    token.token
+    case Token.for_scope(scope) do
+      {:ok, token} -> token.token
+      {:error, _} -> ""
+    end
   end
 end
